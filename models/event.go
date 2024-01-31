2a216145f0b1fd3c1c8c72a3963032795f33e190
shohan-pherones <shohan.sub.56@gmail.com>
2024-01-31 12:18:06 +0600

    post: event handler

--- models/event.go
+++ models/event.go
@@ -4,10 +4,10 @@ import "time"
 
 type Event struct {
 	ID          int
-	Name        string
-	Description string
-	Location    string
-	DateTime    time.Time
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
 	UsersID     int
 }
 
@@ -16,4 +16,8 @@ var events = []Event{}
 func (e Event) Save() {
 	// later: add it to a db
 	events = append(events, e)
+}
+
+func GetAllEvents() []Event {
+	return events
 }
