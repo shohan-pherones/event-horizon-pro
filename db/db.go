777b2b325a63c0a28cb5966926fe7ffc710ca832
shohan-pherones <shohan.sub.56@gmail.com>
2024-01-31 12:40:34 +0600

    db: sqlite3 driver

---
+++ db/db.go
@@ -0,0 +1,20 @@
+package db
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	DB, err := sql.Open("sqlite3", "api.db")
+
+	if err != nil {
+		panic("Could not connnect to database.")
+	}
+
+	DB.SetMaxOpenConns(10)
+	DB.SetMaxIdleConns(5)
+}
