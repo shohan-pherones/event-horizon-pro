e4f6b46118d5253a2a42c3951061384fa04576eb
shohan-pherones <shohan.sub.56@gmail.com>
2024-02-01 13:42:09 +0600

    token verification

--- routes/events.go
+++ routes/events.go
@@ -35,6 +35,13 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
+		return
+	}
+
 	var event models.Event
 	err := context.ShouldBindJSON(&event)
 
--- utils/jwt.go
+++ utils/jwt.go
@@ -1,15 +1,16 @@
 package utils
 
 import (
+	"errors"
 	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
-func GenerateToken(email string, userId int64) (string, error) {
-	secretKey := os.Getenv("JWT_SECRET")
+var secretKey = os.Getenv("JWT_SECRET")
 
+func GenerateToken(email string, userId int64) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"email":  email,
 		"userId": userId,
@@ -17,4 +18,35 @@ func GenerateToken(email string, userId int64) (string, error) {
 	})
 
 	return token.SignedString([]byte(secretKey))
+}
+
+func VerifyToken(token string) error {
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return errors.New("unexpected signing method")
+	}
+
+	isTokenValide := parsedToken.Valid
+
+	if !isTokenValide {
+		return errors.New("invalid token")
+	}
+
+	// claims, ok := parsedToken.Claims.(jwt.MapClaims)
+
+	// if !ok {
+	// 	return errors.New("invalid token claims")
+	// }
+
+	// email := claims["email"].(string)
+	// userId := claims["userId"].(int64)
+
+	return nil
 }
